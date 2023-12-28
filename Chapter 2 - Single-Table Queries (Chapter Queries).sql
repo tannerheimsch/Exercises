abb715480d1dcc099eb3454d6a612cb6f59c0f89
tannerheimsch <h3imscht@outlook.com>
2023-12-27 18:17:12 -0600

    Chapter 2 worked through p.49

--- Chapter 2 - Single-Table Queries (Chapter Queries).sql
+++ Chapter 2 - Single-Table Queries (Chapter Queries).sql
@@ -1,3 +1,4 @@
+-- Elements of the SELECT statement  --
 -- Query to filter orders placed by Customer 71, grouped by employee & year, and then filter only groups of employees and years that have more than one order
 USE TSQLV6;
 
@@ -27,15 +28,21 @@ Explanation:
 6. Orders (sorts) the rows in the output by employee ID and order year
 */
 
+
+-- The FROM clause --
 -- Query to get all rows from the Orders table in the Sales schema, selecting the attributes: 'orderid', 'custid', 'empid', 'orderdate', and 'freight'
 SELECT orderid, custid, empid, orderdate, freight
 FROM Sales.Orders;
 
+
+-- The WHERE clause  --
 -- Query to visualize the WHERE phase
 SELECT orderid, empid, orderdate, freight
 FROM Sales.Orders
 WHERE custid = 71;
 
+
+-- The GROUP BY clause -- 
 -- Query to return 16 rows for the 16 groups of employee-ID and order-year values
 SELECT empid, YEAR(orderdate) AS orderyear
 FROM Sales.Orders
@@ -60,6 +67,8 @@ SELECT
 FROM Sales.Orders
 GROUP BY empid, YEAR(orderdate);
 
+
+-- The HAVING clause -- 
 -- Query to return the 9 groups created by the GROUP BY phase to only show rows with more than 1 order
 SELECT empid, YEAR(orderdate) AS orderyear
 FROM Sales.Orders
@@ -67,6 +76,8 @@ WHERE custid = 71
 GROUP BY empid, YEAR(orderdate)
 HAVING COUNT(*) > 1;
 
+
+-- The SELECT clause -- 
 -- Query to return the 9 rows (one for each group) with the aggregate function the SELECT clause
 SELECT empid, YEAR(orderdate) AS orderyear, COUNT(*) AS numorders
 FROM Sales.Orders
@@ -78,3 +89,57 @@ HAVING COUNT(*) > 1;
 SELECT empid, YEAR(orderdate) AS orderyear
 FROM Sales.Orders
 WHERE custid = 71;
+
+-- Query to limit the multiset query above to remove duplicates
+SELECT DISTINCT empid, YEAR(orderdate) AS orderyear
+FROM Sales.Orders
+WHERE custid = 71;
+
+
+-- The ORDER BY Clause
+-- Query demonstrating the ORDER BY clause, sorted by employee ID and order year
+SELECT empid, YEAR(orderdate) AS orderyear, COUNT(*) AS numorders
+FROM Sales.Orders
+WHERE custid = 71
+GROUP BY empid, YEAR(orderdate)
+HAVING COUNT(*) > 1
+ORDER BY empid, orderyear;
+
+-- Query to sort the employee rows by hire date WITHOUT returning the 'hiredate' attribute
+SELECT empid, firstname, lastname, country
+FROM HR.Employees
+ORDER BY hiredate;
+
+
+-- The TOP and OFFSET-FETCH filters
+-- The TOP filter
+-- Query to return Top 5 most recent orders from the Orders table
+SELECT TOP(5) orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+-- Query to request the top 1 percent of the most recent orders
+SELECT TOP(1) PERCENT orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+-- Query demonstrating TOP with unique ORDER BY list to be deterministic so the row with the greater 'orderid' value will be preferred in the result
+SELECT TOP(5) orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC;
+
+-- Query requesting all ties/rows from the table be returned that have the same sort value (orderdate) as the last one found
+SELECT TOP(5) WITH TIES orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+-- The OFFSET-FETCH filter
+-- Query ordering rows from the Orders table based on the 'orderdate' and 'orderid' attributes from least recent > most recent with 'orderid' as the tiebreaker
+-- The OFFSET clause skips the first 50 rows and the FETCH clause filters the next 25 rows only
+SELECT orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate, orderid
+OFFSET 50 ROWS FETCH NEXT 25 ROWS ONLY;
+
+
+-- Window Functions (quick look)
