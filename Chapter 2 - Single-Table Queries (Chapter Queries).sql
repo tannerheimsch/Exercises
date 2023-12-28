a20e41f6332156d938bee25d8b84935b9327c041
tannerheimsch <h3imscht@outlook.com>
2023-12-27 22:36:08 -0600

    Chapter 2 worked through p.53

--- Chapter 2 - Single-Table Queries (Chapter Queries).sql
+++ Chapter 2 - Single-Table Queries (Chapter Queries).sql
@@ -143,3 +143,91 @@ OFFSET 50 ROWS FETCH NEXT 25 ROWS ONLY;
 
 
 -- Window Functions (quick look)
+-- Query to visualize row numbers, creating a separate partition for each distinct 'custid' value.
+SELECT orderid, custid, val,
+	ROW_NUMBER() OVER(PARTITION BY custid
+										ORDER BY val) AS rownum
+FROM Sales.OrderValues
+ORDER BY custid, val;
+
+/* The LOGICAL ORDER in which all clauses discussed above are processed:
+- FROM
+- WHERE
+- GROUP BY
+- HAVING
+- SELECT
+   - Expressions
+   - DISTINCT
+- ORDER BY
+   - TOP/OFFSET-FETCH
+*/
+
+
+-- Predicates and Operators --
+-- **Data Types, Precedence, and Related Information: https://learn.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver16**
+
+-- Query to return orders in which the order ID is equal to 10248, 10249, or 10250 (IN predicate)
+SELECT orderid, empid, orderdate
+FROM Sales.Orders
+WHERE orderid IN(10248, 10249, 10250);
+
+-- Query to return all orders in the inclusive range 10300 through 10310 (BETWEEN predicate)
+SELECT orderid, empid, orderdate
+FROM Sales.Orders
+WHERE orderid BETWEEN 10300 AND 10310;
+
+-- Query to return employees whose last names tart with the letter D (LIKE predicate)
+SELECT empid, firstname, lastname
+FROM HR.Employees
+WHERE lastname LIKE N'D%';
+
+-- Query to return all orders place on or after January 1, 2022 (comparison operators)
+SELECT orderid, empid, orderdate
+FROM Sales.Orders
+WHERE orderdate >= '20220101';
+
+-- Query to return all orders placed on or after January 1, 2022 that were handled by an employee whose ID is other than 1, 3, and 5 (logical expression combination)
+SELECT orderid, empid, orderdate
+FROM Sales.Orders
+WHERE orderdate >= '20220101'
+	AND empid NOT IN(1, 3, 5);
+
+-- Query to calculate the net value as a result of arithmetic manipulation of the 'quantity', 'unitprice', and 'discount' attributes. (arithmetic operators)
+SELECT orderid, productid, qty, unitprice, discount,
+	qty * unitprice * (1 - discount) AS val
+FROM Sales.OrderDetails;
+
+
+/* Operator Evaluation Precedence Rules (Highest -> Lowest)
+
+1. () [Parentheses]
+2. * [Multiplication], / [Division], % [Modulo]
+3. + [Positive], - [Negative], + [Addition], + [Concatenation], – [Subtraction]
+4. =, >, <, >=, <=, <>, !=, !>, !< [Comparison Operators]
+5. NOT
+6. AND
+7. BETWEEN, IN, LIKE, OR
+8. = [Assignment]
+*/
+
+-- Query to return orders that were either "placed by customer 1 and handled by employees 1, 3, or 5" OR "placed by customer 85 and handled by employees 2, 4, 6" (AND has precedence over OR)
+SELECT orderid, custid, empid, orderdate
+FROM Sales.Orders
+WHERE
+				custid = 1
+		AND empid IN(1, 3, 5)
+		OR custid = 85
+		AND empid IN(2, 4, 6);
+
+-- Query using parentheses to force precedence with logical operators (same as above - better readability)
+SELECT orderid, custid, empid, orderdate
+FROM Sales.Orders
+WHERE
+			(			custid = 1
+				AND empid IN(1, 3, 5) )
+		OR
+			(			custid = 85
+				AND empid IN(2, 4, 6) );
+
+
+-- CASE Expressions --
